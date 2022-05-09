b5d26bff19655f4a9355e9086acbd07f0b27db7a
Litv1n <litv1n.true@gmail.com>
2022-05-09 13:32:38 +0300

    Add API documentation.

--- app/app/urls.py
+++ app/app/urls.py
@@ -3,9 +3,31 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Service app API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.app.com/policies/terms/",
+        contact=openapi.Contact(email="test@test.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/restaurant/', include('restaurant.urls')),
-    path('api/menu/', include('menu.urls'))
+    path('api/menu/', include('menu.urls')),
+    path('docs/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
